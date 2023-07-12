3fc6e335416d04ba0cd9c59f31e1a36d7feff7e2
Vladi Mihtarski <132908375+VladiMihtarski@users.noreply.github.com>
2023-07-12 14:21:07 +0300

    07_Recursive_Fibonacci.py

--- 07_Recursive_Fibonacci.py
+++ 07_Recursive_Fibonacci.py
@@ -1,16 +1,25 @@
+fibonacci_cache = {}
+
+
 def getFibonacci(n):
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+
     if n == 0:
-        return 0
+        fibonacci_cache[n] = 0
     elif n == 1:
-        return 1
+        fibonacci_cache[n] = 1
     else:
-        return getFibonacci(n-1) + getFibonacci(n-2)
+        fibonacci_cache[n] = getFibonacci(n - 1) + getFibonacci(n - 2)
+
+    return fibonacci_cache[n]
+
 
 # Четене на входното число
 n = int(input())
 
-# Изчисляване на (n+1)-вото число на Фибоначи
-fibonacci_number = getFibonacci(n + 1)
+# Изчисляване на n-тото число на Фибоначи
+fibonacci_number = getFibonacci(n+1)
 
 # Извеждане на резултата
 print(fibonacci_number)
