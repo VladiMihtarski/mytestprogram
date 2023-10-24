28f5007b4978eb01f164f6015cb91e512b4682f5
Vladi Mihtarski <132908375+VladiMihtarski@users.noreply.github.com>
2023-10-24 19:05:14 +0300

    sort.py

---
+++ sort.py
@@ -0,0 +1,13 @@
+# Въвеждане на последователност от числа разделени с интервали
+input_sequence = input("Въведете последователност от числа: ")
+numbers = list(map(int, input_sequence.split()))
+
+# Функция за проверка дали число е четно
+def sort(number):
+    return number
+
+# Използване на filter() за филтриране на четните числа
+even_numbers = list(sorted(numbers))
+
+# Извеждане на резултата
+print("Четните числа са:", even_numbers)
