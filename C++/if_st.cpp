fa54d5eb843e48038e2483cc165acf84296b5c28
Vladi Mihtarski <132908375+VladiMihtarski@users.noreply.github.com>
2024-07-09 13:37:21 +0300

    if_st.cpp

---
+++ C++/if_st.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace str;
+int main()
+{
+    int x, y;
+    cout << "Please enter the First x:" <<endl;
+    cin >> x;
+    cout << "Please enter the Second y:" <<endl;
+    cin >> y;
+    if (x > y)
+        cout << "x is greater than y";
+    else (y > x);
+        cout << "y is greater than x";
+}
